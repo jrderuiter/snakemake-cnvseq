23d68eb8e1e3fc72d7ec2f5a176a14994b7f5940
Julian de Ruiter <julianderuiter@gmail.com>
2017-09-12 11:00:12 +0200

    Updated script.

--- scripts/qdnaseq_annotate.py
+++ scripts/qdnaseq_annotate.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
 
 import argparse
+import logging
 
 from genopandas import GenomicDataFrame, RegionMatrix
+
+logging.basicConfig(
+    format='[%(asctime)-15s]  %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
 
 
 def main():
@@ -11,15 +17,20 @@ def main():
     args = parse_args()
 
     # Read matrix.
+    logging.info('Reading matrix values')
     region_values = RegionMatrix.from_csv(
         args.input, sep='\t', expand_index=True)
 
     # Annotate with genes.
+    logging.info('Reading gene annotation')
     genes = GenomicDataFrame.from_gtf(
-        args.gtf, filter=lambda rec: rec.feature == 'gene')
+        args.gtf, filter_=lambda rec: rec.feature == 'gene')
+
+    logging.info('Annotating regions (may take a long time)')
     gene_values = region_values.annotate(genes, id_='gene_name')
 
     # Write output.
+    logging.info('Writing outputs')
     gene_values.values.to_csv(args.output, sep='\t', index=True)
 
 
